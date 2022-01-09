d6d94b818288aa00f77b4c66f4a12c63b7c21891
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2022-01-09 19:02:34 +0600

    Update bot.py

--- helper/bot.py
+++ helper/bot.py
@@ -1,5 +1,5 @@
 import telegram.ext as tg
-from . import *
+from .config import BOT_TOKEN
 
 updater = tg.Updater(token=BOT_TOKEN, request_kwargs={'read_timeout': 30, 'connect_timeout': 10})
 bot = updater.bot
