996a009572d7cd9247d84ab9ffd1ee4ef8eb7357
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2022-01-09 18:04:09 +0600

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="https://github.com/tiktikimelbo7/compressorbot"),
+                Button.url("DEVELOPER", url="t.me/tonmoy_spark"),
             ],
         ],
     )
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="https://github.com/tiktikimelbo7/compressorbot"),
+                Button.url("DEVELOPER", url="t.me/tonmoy_spark"),
             ],
         ],
     )
