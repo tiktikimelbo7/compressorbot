ecca68a95597d1928333a190dc7324e9630058ea
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2022-01-09 18:14:56 +0600

    Create bot_commands.py

---
+++ helper/bot_commands.py
@@ -0,0 +1,7 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.HelpCommand = 'help'
+        self.PingCommand = 'ping'
+
+BotCommands = _BotCommands()
